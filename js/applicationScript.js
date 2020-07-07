5b267dffe09b93589e7925cdbbe2ad7c08a1fcd5
PhilCAEUser <mail@rwth>
2020-07-07 11:43:47 +0000

    ApplicationScript edited by Philipp Dolif

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -42,7 +42,9 @@ var init = function() {
 
   client = new Las2peerWidgetLibrary("$Microservice_Url$/todo", iwcCallback, '*');
 
-  client.sendRequest("") 
+  client.sendRequest("GET", "todo", "", "", {}, false, function(data, type) { 
+    console.log(data);
+}); 
 
 
 };
